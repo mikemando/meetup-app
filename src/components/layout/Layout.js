f45678731bb1c803b28763cbd3e70c592ed19ae7
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2021-05-13 13:47:17 -0800

    Included form to the new-meetup route and modified styles

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import MainNavigation from "./MainNavigation";
-import classes from "./Layout.modules.css";
+import classes from "./Layout.module.css";
 
 function Layout(props) {
     return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetups</div>;
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
+    );
 }
 
 export default NewMeetupPage;
